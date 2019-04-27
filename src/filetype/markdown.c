05708a4dbdf30b87ecc50aa90520aa1dc75d4fe8
wunderfeyd <wunderfeyd@datenstrom.se>
2019-04-27 20:45:25 +0200

    Updated markdown highlighter.

--- src/filetype/markdown.c
+++ src/filetype/markdown.c
@@ -32,21 +32,41 @@ const char* file_type_markdown_name(void) {
 void file_type_markdown_mark(struct document_text_render_info* render_info) {
   codepoint_t cp1 = render_info->sequence->cp[0];
   codepoint_t cp2 = unicode_sequencer_find(&render_info->sequencer, 1)->cp[0];
+  codepoint_t cp3 = unicode_sequencer_find(&render_info->sequencer, 2)->cp[0];
 
   int before = render_info->visual_detail;
   if (before&VISUAL_DETAIL_NEWLINE) {
-    before &= ~(VISUAL_DETAIL_STRING0|VISUAL_DETAIL_STRING1|VISUAL_DETAIL_STRING2);
+    before &= ~(VISUAL_DETAIL_STRING0|VISUAL_DETAIL_STRING1|VISUAL_DETAIL_STRING2|VISUAL_DETAIL_COMMENT0);
   }
 
   int before_masked = before&(VISUAL_DETAIL_STATEMASK&~VISUAL_DETAIL_STRING0);
   int after = before&~(VISUAL_DETAIL_INDENTATION|VISUAL_DETAIL_WORD);
+  int flags = 0;
 
   if (cp1=='\t' || cp1==' ') {
+    after &= ~VISUAL_DETAIL_STRING0;
     after |= VISUAL_DETAIL_INDENTATION;
+  } else if ((cp1>='0' && cp1<='9') && !(before&VISUAL_DETAIL_STOPPED_INDENTATION)) {
+    for (size_t n = 0; n<10; n++) {
+      codepoint_t cp = unicode_sequencer_find(&render_info->sequencer, n+1)->cp[0];
+      if (cp=='.') {
+        flags = VISUAL_FLAG_COLOR_STRING;
+        render_info->keyword_length = (int)n+2;
+        break;
+      } else if (cp<'0' || cp>'9') {
+        after |= VISUAL_DETAIL_WORD;
+        break;
+      }
+    }
   } else if ((cp1>='a' && cp1<='z') || (cp1>='A' && cp1<='Z') || (cp1>='0' && cp1<='9') || cp1=='_') {
     after |= VISUAL_DETAIL_WORD;
-  } else if ((cp1=='*' || cp1=='-') && (cp2==' ' || cp2=='\t') && !(before&VISUAL_DETAIL_STOPPED_INDENTATION)) {
+  } else if ((cp1=='*' || cp1=='-' || cp1=='+') && (cp2==' ' || cp2=='\t') && !(before&VISUAL_DETAIL_STOPPED_INDENTATION)) {
+    flags = VISUAL_FLAG_COLOR_STRING;
+    render_info->keyword_length = 1;
+  } else if ((cp1=='=' || cp1=='-' || cp1=='#') && !(before&VISUAL_DETAIL_STOPPED_INDENTATION)) {
     after |= VISUAL_DETAIL_STRING0;
+  } else if ((cp1=='>') && !(before&VISUAL_DETAIL_STOPPED_INDENTATION)) {
+    after |= VISUAL_DETAIL_COMMENT0;
   } else if (cp1=='[') {
     if (before_masked==0) {
       after |= VISUAL_DETAIL_STRING1;
@@ -55,6 +75,8 @@ void file_type_markdown_mark(struct document_text_render_info* render_info) {
     if (before_masked==VISUAL_DETAIL_STRING1) {
       after &= ~VISUAL_DETAIL_STRING1;
     }
+  } else if (cp1=='`' && cp2=='`' && cp3=='`') {
+    after ^= VISUAL_DETAIL_COMMENT1;
   } else if (cp1=='`') {
     if (before_masked==0) {
       after |= VISUAL_DETAIL_STRING2;
@@ -63,12 +85,17 @@ void file_type_markdown_mark(struct document_text_render_info* render_info) {
     }
   }
 
-  int flags = 0;
   if ((before&after)&VISUAL_DETAIL_STRING1) {
     flags = VISUAL_FLAG_COLOR_PREPROCESSOR;
     render_info->keyword_length = 1;
-  } else if (before&VISUAL_DETAIL_STRING0) {
-    flags = VISUAL_FLAG_COLOR_STRING;
+  } else if ((before|after)&VISUAL_DETAIL_STRING0) {
+    flags = VISUAL_FLAG_COLOR_TYPE;
+    render_info->keyword_length = 1;
+  } else if ((before|after)&VISUAL_DETAIL_COMMENT1) {
+    flags = VISUAL_FLAG_COLOR_BLOCKCOMMENT;
+    render_info->keyword_length = (after&VISUAL_DETAIL_COMMENT1)?1:3;
+  } else if ((before|after)&VISUAL_DETAIL_COMMENT0) {
+    flags = VISUAL_FLAG_COLOR_LINECOMMENT;
     render_info->keyword_length = 1;
   }
 
